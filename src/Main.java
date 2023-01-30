ed8c9db37e40d53b384437a23b589efea876939f
Buhajus <domassteimantas@gmail.com>
2023-01-30 18:32:12 +0200

    V.1.4

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,7 @@ public class Main {
         tea.add("Green");
         tea.add("Fruit");
         tea.add("Black");
+        tea.add("Vegan free");
 
         Scanner input = new Scanner(System.in);
         Scanner input2 = new Scanner(System.in);
@@ -58,7 +59,7 @@ public class Main {
                 }
                 System.out.println("\nPlease choose your tea:");
                 typeOfTea = input2.nextLine();
-                System.out.print("You selected:\t" + typeOfTea.toLowerCase() + " tea,");
+                System.out.println("You selected:\t" + typeOfTea.toLowerCase() + " tea");
             }
 
             drinkCost = drinks.get(selectDrink);
@@ -66,7 +67,7 @@ public class Main {
             System.out.println("Please insert coin:");
             totalOfCoins = input2.nextDouble();
             while (totalOfCoins < drinkCost) {
-                System.out.println("Remaining amount " + (drinkCost - totalOfCoins) + " Eur");
+                System.out.println("Remaining amount " + String.format("%.2f", (drinkCost - totalOfCoins)) + " Eur");
                 totalOfCoins += input2.nextDouble();
             }
 
@@ -99,45 +100,3 @@ public class Main {
         }
     }
 }
-
-//TODO: Kavos aparato programa
-// kavos pasirinkimas pagal skaičius : 1 - Latte, 2 - Black coffee ir t.t.
-//
-//Kavos aparatas gamina:
-//- late (3 eur);
-//- juodą kavą (1.50 eur);
-//- baltą kavą (2 eur);
-//- arbatą (1.20 eur).
-//Vartotojas, norėdamas nusipirkti norimą produktą, turi įvesti gaminio pavadinimą, tada įdėti grynų pinigų (aparatas atiduoda gražą).
-//Jeigu vartotojas pasirenka arbatą, tada aparatas dar paklausia kokios pageidauja:
-//- žalios;
-//- vaisinės;
-//- juodos.
-//Pagaminęs produktą aparatas paklausia ar norės kartoti? Jeigu norės, prasideda viskas iš pradžių, priešingu atveju- padėkoja ir palinki gražios dienos.
-//
-//Įvedimo/ išvedimo pavyzdžiai:
-//pirmas pavyzdys
-//[Sistemos pranešimas] Sveiki. Čia kavos aparato programa. Prašome įvesti iš sarašo (duotas aukščiau) norimą produktą:
-//[Vartotojas įveda] balta kava
-//[Sistemos pranešimas] balta kava kainuoja 2 eurus. Prašome įdėti pinigus.
-//[Vartotojas įveda] 1.80
-//[Sistemos pranešimas] per maža suma. Prašome įvesti reikiamą sumą.
-//[Vartotojas įveda] 5
-//[Sistemos pranešimas] balta kava kainuoja 2 eurus. Atiduodu 3 eur grąžą.
-//[Sistemos pranešimas] prašome palaukti- gaminamas užsakymas.
-//[Sistemos pranešimas] prašome pasiimti
-//[Sistemos pranešimas] ar norėsite pakartoti?
-//[Vartotojas įveda] Taip (ir vėl viskas iš pradžių)
-//
-//antras pavyzdys
-//[Sistemos pranešimas] Sveiki. Čia kavos aparato programa. Prašome įvesti iš sarašo (duotas aukščiau) norimą produktą:
-//[Vartotojas įveda] arbata
-//[Sistemos pranešimas] arbata kainuoja 1.2 eur. Prašome įdėti pinigus.
-//[Vartotojas įveda] 1.20
-//[Sistemos pranešimas] kokios arbatos pageidausite? (išvardinamas sąrašas).
-//[Vartotojas įveda] žalia
-//[Sistemos pranešimas] prašome palaukti- gaminamas užsakymas.
-//[Sistemos pranešimas] prašome pasiimti
-//[Sistemos pranešimas] ar norėsite pakartoti?
-//[Vartotojas įveda] Ne
-//[Sistemos pranešimas] Ačiū, kad pirkote. Gražios dienos
